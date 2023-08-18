f08d277aab8cb097ce65f8d3879db5ddebcc5ecf
carofkuba <carofkuba@gmail.com>
2023-08-18 13:08:41 -0300

    hice cambio en start message

--- src/main/java/com/carokuba/musicapp/MusicAppApplication.java
+++ src/main/java/com/carokuba/musicapp/MusicAppApplication.java
@@ -11,7 +11,11 @@ public class MusicAppApplication {
 
 		System.out.println("Music App started");
 
+		System.out.println("hice otro cambio pero quiero stashearlo");
 
+		System.out.println("otro cambio mas");
+
+		System.out.println("hago cambio en start message");
 
 	}
 }
